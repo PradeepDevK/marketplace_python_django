f45cadca131abc96e1cbc4c0193b4dc9fd6c9fc3
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-05-28 21:36:41 +0530

    Added new endpoint to upload/add user from excel file

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -7,10 +7,12 @@ from rest_framework_simplejwt.views import (
 
 from user_app.api.views import (
     registration_view,
+    upload_excel,
 )
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', registration_view, name='register'),
+    path('upload_excel/', upload_excel, name='upload_excel'),
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -4,6 +4,14 @@ from rest_framework import status
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
+import pandas as pd
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
+
 from user_app.api.serializer import (
     RegistrationSerializer
 )
@@ -32,3 +40,30 @@ def registration_view(request):
         else:
             data = serializer.errors
         return Response(data)
+    
+    
+@csrf_exempt
+def upload_excel(request):
+    if request.method == 'POST' and request.FILES['file']:
+        excel_file = request.FILES['file']
+        file_name = default_storage.save(excel_file, ContentFile(excel_file.read()))
+        file_path = default_storage.path(file_name)
+        
+        try:
+            df = pd.read_excel(file_path)
+            
+            for index, row in df.iterrows():
+                User.objects.create_user(
+                    username=row['username'],
+                    password=row['password'],
+                    email=row['email']
+                )
+            # Delete the file after processing
+            default_storage.delete(file_name)
+            return JsonResponse({'message': 'Users created successfully'}, status=201)
+        except Exception:
+            # Delete the file if an error occurred
+            default_storage.delete(file_name)
+            return JsonResponse({'error': str(Exception)}, status=400)
+        
+    return JsonResponse({'error': 'Invalid request'}, status=400)
